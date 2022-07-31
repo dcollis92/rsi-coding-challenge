a7640cc547fc6b63cd3cbb8092240d7444778aa0
dcollis92 <davidphilipcollis@gmail.com>
2022-07-31 10:49:41 -0500

    adjust table styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import { ThemeProvider } from '@mui/material/styles';
-import { Typography } from '@mui/material';
-import theme from './Theme';
-import mockData from './data';
-import React from 'react';
-import MaterialTable from '@material-table/core';
+import { ThemeProvider } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import theme from "./Theme";
+import mockData from "./data";
+import React from "react";
+import MaterialTable from "@material-table/core";
 
 // Icons
 // import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -17,16 +17,44 @@ function App() {
   const [selectedRow, setSelectedRow] = useState(null);
 
   const columns = [
-    { title: 'Affidavit No', field: 'PARTA_TRANSACTION.AFFIDAVITNO', type: 'string' },
-    { title: 'Policy No', field: 'PARTA_TRANSACTION.POLICYNO', type: 'string' },
-    { title: 'Insured Name', field: 'PARTA_TRANSACTION.RISKINSUREDNAME', type: 'string' },
-    { title: 'Type', field: 'PARTA_TRANSACTION.TRANSACTIONTYPE', type: 'string' },
-    { title: 'Premium', field: 'PARTA_TRANSACTION.AMOUNT', type: 'currency' },
-    { title: 'Inception', field: 'PARTA_TRANSACTION.EFFECTIVEDATE', type: 'date' },
-    { title: 'Expiration', field: 'PARTA_TRANSACTION.EXPIRATIONDATE', type: 'date' },
-    { title: 'Batch', field: 'PARTA_TRANSACTION.BATCHID', type: 'numeric' },
-    { title: 'Submitted', field: 'PARTA_TRANSACTION.RECEIVEDATE', type: 'date' },
-    { title: 'Proc State', field: 'PARTA_TRANSACTION.PROCESSEDSTATE', type: 'string' }
+    {
+      title: "Affidavit No",
+      field: "PARTA_TRANSACTION.AFFIDAVITNO",
+      type: "string",
+    },
+    { title: "Policy No", field: "PARTA_TRANSACTION.POLICYNO", type: "string" },
+    {
+      title: "Insured Name",
+      field: "PARTA_TRANSACTION.RISKINSUREDNAME",
+      type: "string",
+    },
+    {
+      title: "Type",
+      field: "PARTA_TRANSACTION.TRANSACTIONTYPE",
+      type: "string",
+    },
+    { title: "Premium", field: "PARTA_TRANSACTION.AMOUNT", type: "currency" },
+    {
+      title: "Inception",
+      field: "PARTA_TRANSACTION.EFFECTIVEDATE",
+      type: "date",
+    },
+    {
+      title: "Expiration",
+      field: "PARTA_TRANSACTION.EXPIRATIONDATE",
+      type: "date",
+    },
+    { title: "Batch", field: "PARTA_TRANSACTION.BATCHID", type: "numeric" },
+    {
+      title: "Submitted",
+      field: "PARTA_TRANSACTION.RECEIVEDATE",
+      type: "date",
+    },
+    {
+      title: "Proc State",
+      field: "PARTA_TRANSACTION.PROCESSEDSTATE",
+      type: "string",
+    },
   ];
 
   return (
@@ -35,17 +63,27 @@ function App() {
       <MaterialTable
         data={mockData}
         columns={columns}
-        onRowClick={((evt, selectedRow) => setSelectedRow(selectedRow.tableData.id))}
+        onRowClick={(evt, selectedRow) =>
+          setSelectedRow(selectedRow.tableData.id)
+        }
         options={{
-          rowStyle: rowData => ({
-          backgroundColor: (selectedRow === rowData.tableData.id) ? '#e2f1da' : '#FFF'}),
+          headerStyle: {
+            backgroundColor: theme.palette.grid.main.header,
+            color: theme.palette.grid.main.default,
+          },
+          rowStyle: (rowData) => ({
+            backgroundColor:
+              selectedRow === rowData.tableData.id
+                ? theme.palette.grid.main.active
+                : theme.palette.grid.main.default,
+          }),
           search: true,
           columnsButton: true,
           exportButton: true,
           filtering: true,
           sorting: true,
           pageSize: 10,
-          pageSizeOptions: [10,25,50,100]
+          pageSizeOptions: [10, 25, 50, 100],
         }}
       />
     </ThemeProvider>
@@ -53,4 +91,3 @@ function App() {
 }
 
 export default App;
-
